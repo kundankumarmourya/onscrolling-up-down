252a017aded6754e1a420612b5a4c4fd1a4bffb0
Kundan Kumar Mourya <32189172+kundankumarmourya@users.noreply.github.com>
2018-06-16 16:51:59 +0530

    mouse scrolling up and down
    On mouse scrolling up and down  adding & removing class

---
+++ mouse-scrolling-up-down.js
@@ -0,0 +1,18 @@
+
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop;
+   if (st > lastScrollTop || st == 0) //or window top zero {
+        // downscroll code
+     $(".main-menu").removeClass("show-menu");
+       $(".main-menu").addClass("hide-menu");
+      
+   } else {
+       // Upscroll code
+       $(".main-menu").addClass("show-menu");
+        $(".main-menu").removeClass("hide-menu");
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
