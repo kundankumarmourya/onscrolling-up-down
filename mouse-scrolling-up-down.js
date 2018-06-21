13c4c681d77188989886e8240d9bdad23e11ae16
Kundan Kumar Mourya <32189172+kundankumarmourya@users.noreply.github.com>
2018-06-21 18:20:15 +0530

    wrong place comment

--- mouse-scrolling-up-down.js
+++ mouse-scrolling-up-down.js
@@ -3,7 +3,8 @@ var lastScrollTop = 0;
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
    var st = window.pageYOffset || document.documentElement.scrollTop;
-   if (st > lastScrollTop || st == 0) //or window top zero {
+   if (st > lastScrollTop || st == 0) //or window top zero 
+   {
         // downscroll code
      $(".main-menu").removeClass("show-menu");
        $(".main-menu").addClass("hide-menu");
